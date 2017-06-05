8f2353e70cc5ad5befcb3fb1fa961a093f3207e6
zhan <senyou2012@163.com>
2017-06-06 00:27:56 +0800

    no message

---
+++ ZHYShareView-master/ZHYShareView-master/ViewController.m
@@ -0,0 +1,74 @@
+//
+//  ViewController.m
+//  ZHYShareView-master
+//
+//  Created by zhan on 2017/6/5.
+//  Copyright © 2017年 zhan. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "ZHYShareView.h"
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (IBAction)show
+{
+    ZHYShareItem *item1 = [ZHYShareItem initWithImage:@"" title:@"cell1" type:@1 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item2 = [ZHYShareItem initWithImage:@"" title:@"cell2" type:@2 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item3 = [ZHYShareItem initWithImage:@"" title:@"cell3" type:@3 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item4 = [ZHYShareItem initWithImage:@"" title:@"cell4" type:@4 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item5 = [ZHYShareItem initWithImage:@"" title:@"cell5" type:@5 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item6 = [ZHYShareItem initWithImage:@"" title:@"cell6" type:@6 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item7 = [ZHYShareItem initWithImage:@"" title:@"cell7" type:@7 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item8 = [ZHYShareItem initWithImage:@"" title:@"cell8" type:@8 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item9 = [ZHYShareItem initWithImage:@"" title:@"cell9" type:@9 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    ZHYShareItem *item10 = [ZHYShareItem initWithImage:@"" title:@"cell10" type:@10 selectedHandler:^(id value){
+        NSLog(@"item4%@",value);
+    }];
+    
+    [ZHYShareView showViewWithItems:@[item1,item2,item3,item4,item5,item6,item7,item8,item9,item10,item7,item8]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ ZHYShareView-master/ZHYShareView-master/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZHYShareView-master
+//
+//  Created by zhan on 2017/6/5.
+//  Copyright © 2017年 zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
